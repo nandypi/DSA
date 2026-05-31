49b2f2c98e9197cfb0f243b5664874fbbedda038
NandanReddy Parnapalli <117740904+nandypi@users.noreply.github.com>
2026-05-31 23:49:29 +0530

    Time: 2 ms (17.51%) | Memory: 19.4 MB (63.74%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,24 +1,20 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
 
-        def dfs(mat, r, c, oc, uc, v):
+        def dfs(mat, r, c, oc, uc):
 
-            print(r, c, oc, uc)
-
-            if r >= len(mat) or c >= len(mat[0]) or r < 0 or c < 0 or mat[r][c] != oc or (r,c) in v:
+            if r >= len(mat) or c >= len(mat[0]) or r < 0 or c < 0 or mat[r][c] != oc or mat[r][c] == uc:
                 return mat
             
             mat[r][c] = uc
-            v.add((r,c))
 
-            mat = dfs(mat, r-1, c, oc, uc, v) # up
-            mat = dfs(mat, r, c+1, oc, uc, v) # right
-            mat = dfs(mat, r, c-1, oc, uc, v) # left
-            mat = dfs(mat, r+1, c, oc, uc, v) # down
+            mat = dfs(mat, r-1, c, oc, uc) # up
+            mat = dfs(mat, r, c+1, oc, uc) # right
+            mat = dfs(mat, r, c-1, oc, uc) # left
+            mat = dfs(mat, r+1, c, oc, uc) # down
             
             return mat
         
-        visited = set()
-        mat = dfs(image, sr, sc, image[sr][sc], color, visited)
+        mat = dfs(image, sr, sc, image[sr][sc], color)
 
         return mat
