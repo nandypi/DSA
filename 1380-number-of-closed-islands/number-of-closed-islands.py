026cd2fdef21a40a9fbf5d3e6d49197b273c84bc
NandanReddy Parnapalli <117740904+nandypi@users.noreply.github.com>
2026-06-02 13:46:19 +0530

    Time: 13 ms (84.54%) | Memory: 19.7 MB (89.62%) - LeetSync

--- 1380-number-of-closed-islands/number-of-closed-islands.py
+++ 1380-number-of-closed-islands/number-of-closed-islands.py
@@ -3,11 +3,11 @@ class Solution:
         ROWS = len(grid); COLUMNS = len(grid[0])
 
         def markIt_dfs(mat, r, c):
-            if r < 0 or r >= ROWS or c < 0 or c >= COLUMNS or mat[r][c] == 1:
-                return None
-
-            elif (r == 0 or c == 0 or r == ROWS-1 or c == COLUMNS-1) and mat[r][c] == 0:
+            if (r == 0 or c == 0 or r == ROWS-1 or c == COLUMNS-1) and mat[r][c] == 0:
                 return False
+
+            if mat[r][c] == 1:
+                return True
 
             mat[r][c]= 1
 
@@ -16,13 +16,12 @@ class Solution:
             U = markIt_dfs(mat, r-1, c)
             L = markIt_dfs(mat, r, c-1)
 
-            if False in (R, D, U, L):
-                return False
-            return True
+            return False not in (R, D, U, L)
         
         count = 0
         for r in range(ROWS):
             for c in range(COLUMNS):
-                if markIt_dfs(grid, r, c):
-                    count += 1
+                if grid[r][c] == 0:
+                    if markIt_dfs(grid, r, c):
+                        count += 1
         return count
